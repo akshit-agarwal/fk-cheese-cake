8e49e13a1a9b180cd2810beed18678a6f1824b17
raman.khattar <raman.khattar@flipkart.com>
2013-09-12 17:04:31 +0530

    starting rails project

---
+++ fk-cheese-cake/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FkCheeseCake::Application.config.secret_token = '43d63b487438fe1e4a778fe230013ae9c7091d5443430c1223b6b9151301e7a4ca9720425d088cc793fd054e84b00856aa347a7eeba300383890f0df5ea685c2'
